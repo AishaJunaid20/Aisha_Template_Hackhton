6680f9437c9ed3b0c84ca8d9e0a923bc97272fbb
AishaJunaid20 <ayeshajunaidmemon@gmail.com>
2024-12-08 23:48:26 +0500

    Update FoodItems.tsx

--- comoponent/FoodItems.tsx
+++ comoponent/FoodItems.tsx
@@ -1,5 +1,5 @@
 // app/components/FoodItems.tsx
-
+import Image from "next/Image";
 export default function FoodItems() {
     return (
       <section className="py-16 bg-black">
@@ -8,22 +8,22 @@ export default function FoodItems() {
           <h2 className="text-6xl font-bold text-white mb-12 hover:text-yellow-600 font-sans">Choose Your Food Item</h2>
           <div className="grid grid-rows-1 grid-cols-4 gap-6  ">
             <div className="pt-5 gap-6 shadow-lg">
-              <img src="/food1.png" alt="Food 1" className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
+              <Image src="/food1.png" alt="Food 1" width={900px} height={250px} className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
              
              
             </div>
             <div className=" pt-5 gap-6 shadow-lg">
-              <img src="/food2.png" alt="Food 2" className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
+              <Image src="/food2.png" alt="Food 2" width={900px} height={250px} className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
              
               
             </div>
             <div className="pt-5 gap-6  shadow-lg">
-              <img src="/food3.png" alt="Food 3" className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
+              <Image src="/food3.png" alt="Food 3" width={900px} height={250px} className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
               
              
             </div>
             <div className=" pt-5 gap-6  shadow-lg">
-              <img src="/food4.png" alt="Food 3" className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
+              <Image src="/food4.png" alt="Food 3" width={900px} height={250px} className="w-[900px] h-[250px] object-cover mb-4 rounded-lg" />
               
              
             </div>
